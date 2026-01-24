ea3514fc494e1b2e5aaec5d3dba5b3afafe2916a
Melssaid <m.melssaid@gmail.com>
2026-01-24 16:01:11 +0300

    Wrap App with SettingsProvider for centralized settings

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n";
+import { SettingsProvider } from "@/providers/SettingsProvider";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <SettingsProvider>
+    <App />
+  </SettingsProvider>
+);
